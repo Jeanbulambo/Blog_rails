a020b4d668d5f2cdcbf4872585b57c17474065bd
Jean Bulambo <jeanbulambo4@gmail.com>
2022-11-10 21:46:37 +0200

    fix testing errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+    before(:each) do
+      get users_path
+    end
+
+    it 'Check if response status was correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'Check if a correct template was rendered' do
+      expect(response).to render_template(:index)
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
+    before(:each) do
+      @user = User.create(name: 'Integration test', photo: 'http://twitter.com', bio: 'test for User')
+      get user_path(@user)
+    end
+
+    it 'Check if response status was correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'Check if a correct template was rendered' do
+      expect(response).to render_template(:show)
     end
   end
 end
